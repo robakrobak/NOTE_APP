25544849026120c5eda6d3a664bf01746886591d
pprzychodzen <przychu@gmail.com>
2019-10-27 10:20:37 +0100

    404.html added. Fully works only with DEBUG=False and local host added in settings

--- NoteApp/settings.py
+++ NoteApp/settings.py
@@ -45,7 +45,7 @@ ROOT_URLCONF = 'NoteApp.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'core/templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
