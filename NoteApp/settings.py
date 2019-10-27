703ab4cefcba9387013ad7422c228dcb2064003e
Paulina_Bujnik <bujnik.paulina@gmail.com>
2019-10-27 11:10:10 +0100

    Rejestracja_v1

--- NoteApp/settings.py
+++ NoteApp/settings.py
@@ -45,7 +45,7 @@ ROOT_URLCONF = 'NoteApp.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'core/templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -109,3 +109,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
--- NoteApp/urls.py
+++ NoteApp/urls.py
@@ -16,6 +16,11 @@ Including another URLconf
 from django.conf.urls import url
 from django.contrib import admin
 
+from user.views import signup
+
+
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^signup/$', signup, name='signup'),
+
 ]
