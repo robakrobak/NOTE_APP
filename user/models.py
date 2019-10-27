d13bbb1b9fb872d79d110f000e91965473b9b469
robakrobak <robakrobak@protonmail.com>
2019-10-27 11:02:14 +0100

    ARCHITEKTURA PROJEKTU

--- user/models.py
+++ user/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.DO_NOTHING)
+    awatar = models.FileField(upload_to='awatar')
+    about_me = models.CharField(max_length=512)
+
+    def __str__(self):
+        return self.user.username
+
+
