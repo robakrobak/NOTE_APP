cee3dd4622d1ed5b6c34deb48d7297074039b87c
Bartosz Gorny <bartoszgorny1234@gmail.com>
2019-11-08 12:50:57 +0100

    -fix login redirect

--- user/views.py
+++ user/views.py
@@ -11,7 +11,7 @@ def login_view(request):
         if form.is_valid():
             user = form.get_user()
             login(request, user)
-            return render(request, 'index.html', {'form': form})
+            return redirect('home')
     else:
         form = AuthenticationForm()
     return render(request, 'login.html', {'form': form})
