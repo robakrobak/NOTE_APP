30a6e5139d9f685c4492c892a5c63182adc61264
pprzychodzen <przychu@gmail.com>
2019-11-16 14:17:24 +0100

    Frontend improvment
    all button changed
    
    user profile html added
    
    form for signup and login changed

--- NoteApp/settings.py
+++ NoteApp/settings.py
@@ -9,7 +9,6 @@ EMAIL_HOST_PASSWORD = 'Noteapp123!@#'
 EMAIL_USE_TLS = True
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 
-
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -49,7 +48,10 @@ INSTALLED_APPS = [
     'note',
     'user',
     'bootstrap4',
+    'crispy_forms',
 ]
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
