2d58e5ec8558887ee53a0dea3f9a69e23012b858
robakrobak <robakrobak@protonmail.com>
2019-11-23 14:37:02 +0100

    email reminder for users: who is aassigned to task / note

--- user/models.py
+++ user/models.py
@@ -10,7 +10,7 @@ User._meta.get_field('email')._unique = True
 
 class UserProfile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True, related_name='profile')
-    awatar = models.FileField(upload_to='awatar', blank=True)
+    awatar = models.FileField(upload_to='awatar', blank=True, default='media/awatar/default-100.png')
     about_me = models.CharField(max_length=512, blank=True)
     location = models.CharField(max_length=30, blank=True)
     birth_date = models.DateField(null=True, blank=True)
