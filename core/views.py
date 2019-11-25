2a3f966f909ca8802501a7181bca87930e316d4c
bartoszgorny1234 <bartoszgorny1234@gmail.com>
2019-11-25 13:29:52 +0100

    Archive Class change

--- core/views.py
+++ core/views.py
@@ -12,13 +12,13 @@ from note.models import Note
 class NotesListView(ListView):
     model = Note
     template_name = "home.html"
-    ordering = ['deadline']
+    done = False
 
     def get_queryset(self):
         if self.request.user.is_authenticated:
             queryset = super().get_queryset()
             return queryset.filter(Q(id_users=self.request.user) | Q(created_by=self.request.user),
-                                   is_done=False).distinct()
+                                   is_done=self.done).distinct()
         else:
             return Note.objects.none()
 
@@ -33,20 +33,7 @@ def logout_view(request):
     return redirect('/')
 
 
-class NotesListArchiveView(ListView):
+class NotesListArchiveView(NotesListView):
     model = Note
     template_name = "archive.html"
-    ordering = ['deadline']
-
-    def get_queryset(self):
-        if self.request.user.is_authenticated:
-            queryset = super().get_queryset()
-            return queryset.filter(Q(id_users=self.request.user) | Q(created_by=self.request.user),
-                                   is_done=True).distinct()
-        else:
-            return Note.objects.none()
-
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context['filter'] = NoteFilter(self.request.GET, queryset=self.get_queryset())
-        return context
+    done = True
