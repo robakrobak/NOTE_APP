11dab9f26cdd09788c36492795fc1708a4a3e99c
pprzychodzen <przychu@gmail.com>
2019-11-26 22:26:47 +0100

    Note details and comment frontend
    Global font set to open sans
    
    deadline with time

--- note/forms.py
+++ note/forms.py
@@ -1,5 +1,7 @@
 from bootstrap_datepicker_plus import DateTimePickerInput
 from django import forms
+from django.forms.widgets import Textarea
+
 
 from note.models import Note, Comment
 
@@ -15,9 +17,13 @@ class NoteForm(forms.ModelForm):
             'note': forms.Textarea(),
         }
 
-        
+
 class CommentForm(forms.ModelForm):
+
     class Meta:
         model = Comment
         fields = ['text']
-
+        labels = {'text': '' }
+        # widgets = {
+        #     'text': forms.Textarea(),
+        # }
